57a2b8fdc71cb78965393533cc6ae258dafa809c
MichelleRankin <89100717+MichelleRankin@users.noreply.github.com>
2021-09-09 12:53:49 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+$('button').click(function() {
+  var element = $(this);
+  var tmpClass = element.attr('class');
+  element.removeClass();
+  setTimeout(function() {
+    element.offsetWidth = element.offsetWidth;
+    element.addClass(tmpClass).addClass('start-now');
+  }, 1);
+});
